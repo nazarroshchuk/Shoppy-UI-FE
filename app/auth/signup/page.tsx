fe6506bd52b5f8c5e452b1cde3556e12f3094992
n.roshchuk <n.roshchuk@levi9.com>
2025-04-14 12:49:02 +0200

    first commit

---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,15 @@
+import {Button, Link, Stack, TextField} from "@mui/material";
+import NextLink from "next/link";
+
+export default function Signup() {
+    return (
+        <Stack spacing={2} className='w-full max-w-xs'>
+            <TextField type='email' label='Email' placeholder='Email' variant='outlined'/>
+            <TextField type='password' label='Password' placeholder='Password' variant='outlined'/>
+            <Button type='submit' variant='contained' color='primary'>Signup</Button>
+            <Link component={NextLink} href='/auth/login' className='self-center'>
+                Login
+            </Link>
+        </Stack>
+    )
+}
